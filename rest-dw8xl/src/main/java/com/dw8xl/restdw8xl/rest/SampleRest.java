8f5b70a6be048795541fb21eb6c07f166f223acd
Haku Wei <hofneondeion21@gmail.com>
2020-09-07 02:03:24 -0400

    Updated SampleRest.java to include *Rest Instances in Data Field

--- rest-dw8xl/src/main/java/com/dw8xl/restdw8xl/rest/SampleRest.java
+++ rest-dw8xl/src/main/java/com/dw8xl/restdw8xl/rest/SampleRest.java
@@ -3,10 +3,17 @@
  */
 package com.dw8xl.restdw8xl.rest;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Haku Wei
  *
  */
+@RestController("/DynasyWarror8XL")
 public class SampleRest {
 
+	private WeaponRest weaponRest;
+	private KingdomRest kingdomRest;
+	private CharacterRest characterRest;
+	
 }
