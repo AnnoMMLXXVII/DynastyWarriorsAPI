843265c40789a4358c70988495ce8709891b67f3
Haku Wei <hofneondeion21@gmail.com>
2020-09-07 04:08:22 -0400

    Added Type class that can help link the weapon and the character together. Do not need to necessarily be dependant on weapon or character separately

---
+++ rest-dw8xl/src/main/java/com/dw8xl/restdw8xl/model/weapon/Type.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package com.dw8xl.restdw8xl.model.weapon;
+
+/**
+ * @author Haku Wei
+ *
+ */
+public class Type {
+
+	private String type, name;
+	
+	/**
+	 * @param name
+	 */
+	public Type(String name) {
+		super();
+		this.name = name;
+	}
+
+	/**
+	 * @param type
+	 * @param name
+	 */
+	public Type(String type, String name) {
+		super();
+		this.type = type;
+		this.name = name;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Type other = (Type) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Type [type=" + type + ", name=" + name + "]";
+	}
+
+	
+	
+	
+}
