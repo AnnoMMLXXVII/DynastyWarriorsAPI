acb3278e60531e4bd171a0bee24edd8ac1e29ab7
Haku Wei <hofneondeion21@gmail.com>
2020-09-10 15:13:42 -0400

    Added new SampleListController will generate sample lists of officers, weapons, etc

---
+++ rest-dw8xl/src/main/java/com/dw8xl/restdw8xl/Controller/SampleListController.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.dw8xl.restdw8xl.Controller;
+
+import java.util.List;
+
+import com.dw8xl.restdw8xl.model.character.CharacterI;
+import com.dw8xl.restdw8xl.model.kingdom.KingdomI;
+import com.dw8xl.restdw8xl.model.weapon.Affinity;
+import com.dw8xl.restdw8xl.model.weapon.Type;
+import com.dw8xl.restdw8xl.model.weapon.Weapon;
+import com.dw8xl.restdw8xl.model.weapon.attribute.AttributeI;
+
+/**
+ * @author Haku Wei
+ *
+ */
+public class SampleListController {
+
+	private List<CharacterI> officers;
+	private List<Type> types;
+	private List<Affinity> affinities;
+	private List<KingdomI> kingdoms;
+	private List<Weapon> weapons;
+	private List<List<AttributeI>> attributes;
+	
+	
+}
