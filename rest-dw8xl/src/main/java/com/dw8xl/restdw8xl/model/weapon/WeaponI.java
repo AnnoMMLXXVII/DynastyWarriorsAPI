f1c643bb8bfd5119b292dae6c76f5d97d8350206
Haku Wei <hofneondeion21@gmail.com>
2020-09-20 02:24:31 -0400

    Refactored and Updated WeaponI and WeaponDNE classes

--- rest-dw8xl/src/main/java/com/dw8xl/restdw8xl/model/weapon/WeaponI.java
+++ rest-dw8xl/src/main/java/com/dw8xl/restdw8xl/model/weapon/WeaponI.java
@@ -5,7 +5,10 @@ package com.dw8xl.restdw8xl.model.weapon;
 
 import java.util.List;
 
+import com.dw8xl.restdw8xl.model.weapon.affinity.AffinityI;
 import com.dw8xl.restdw8xl.model.weapon.attribute.AttributeI;
+import com.dw8xl.restdw8xl.model.weapon.category.CategoryI;
+import com.dw8xl.restdw8xl.model.weapon.type.Type;
 
 /**
  * @author Haku Wei
@@ -14,9 +17,16 @@ import com.dw8xl.restdw8xl.model.weapon.attribute.AttributeI;
 public interface WeaponI {
 	
 	public String getName();
-	
-	public List<AttributeI> getAttributes();
-	
+	public Integer getBaseAttk();
+	public Type getType();
 	public Integer getStar();
+	
+	public abstract AffinityI getAffinity();
+	public abstract List<AttributeI> getAttributes();
+	public abstract CategoryI getCategory();
+	public abstract WeaponI getInstance();
+	
+	public int hashCode();
+	public boolean equals(Object obj);
 
 }
