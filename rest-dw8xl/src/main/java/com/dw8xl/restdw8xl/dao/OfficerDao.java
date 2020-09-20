f874f8df517cee9ced1b9f15a1667f046d86e1ae
Haku Wei <hofneondeion21@gmail.com>
2020-09-20 02:28:29 -0400

    Created Empty Dao and DaoI objects

---
+++ rest-dw8xl/src/main/java/com/dw8xl/restdw8xl/dao/OfficerDao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.dw8xl.restdw8xl.dao;
+
+/**
+ * @author Haku Wei
+ *
+ */
+public class OfficerDao {
+
+	/**
+	 * 
+	 */
+	public OfficerDao() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ rest-dw8xl/src/main/java/com/dw8xl/restdw8xl/dao/OfficerDaoI.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.dw8xl.restdw8xl.dao;
+
+/**
+ * @author Haku Wei
+ *
+ */
+public interface OfficerDaoI {
+
+}
