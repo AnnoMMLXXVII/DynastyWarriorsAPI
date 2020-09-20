5ec2b5a3fc9714dda681ab1fdb349954b2f9fe96
Haku Wei <hofneondeion21@gmail.com>
2020-09-20 02:20:00 -0400

    New pacakge. AffinityI interface and AffinityDNE

---
+++ rest-dw8xl/src/main/java/com/dw8xl/restdw8xl/model/weapon/affinity/AffinityDNE.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.dw8xl.restdw8xl.model.weapon.affinity;
+
+/**
+ * @author Haku Wei
+ *
+ */
+public class AffinityDNE implements AffinityI {
+
+	/**
+	 * 
+	 */
+	public AffinityDNE() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getAffinity() {
+		return "";
+	}
+
+	@Override
+	public AffinityI getInstance() {
+		return this;
+	}
+	
+	
+
+}
