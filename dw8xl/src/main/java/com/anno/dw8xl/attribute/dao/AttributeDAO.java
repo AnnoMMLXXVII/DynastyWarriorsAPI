ce4c4aea4ce98a4dbf4830fb3c5c42994879a283
Haku Wei <hofneondeion21@gmail.com>
2020-10-22 03:19:33 -0400

    Added and modified DAO layer

--- dw8xl/src/main/java/com/anno/dw8xl/attribute/dao/AttributeDAO.java
+++ dw8xl/src/main/java/com/anno/dw8xl/attribute/dao/AttributeDAO.java
@@ -10,6 +10,10 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
 import com.anno.dw8xl.attribute.model.AttributeI;
 import com.anno.dw8xl.attribute.model.Normal;
 import com.anno.dw8xl.attribute.model.Special;
@@ -18,20 +22,24 @@ import com.anno.dw8xl.attribute.model.Special;
  * @author Haku Wei
  *
  */
+@Service
 public class AttributeDAO implements AttributeDAO_I{
-	private static final String URL = "C:\\Users\\Haku Wei\\Documents\\git\\DynastyWarriors8XL\\Text-Files\\attributes\\";
-	private static final String NORMAL= "normal\\normal";
-	private static final String SPECIAL= "special\\special";
+	
+	private static final String URL = "src/main/resources/Text-Files/attributes";
+	private static final String NORMAL= "/normal/normal";
+	private static final String SPECIAL= "/special/special";
+	private static final Logger LOGGER = LoggerFactory.getLogger(AttributeDAO.class);
 	private Map<String, AttributeI> mapAttributes;
-	private static AttributeDAO_I instance = null;
+	private static AttributeDAO instance = null;
 	
 	/*
 	 * Singleton to get AttrbuteDAO_I Instance
 	 */
-	public static AttributeDAO_I getInstance() {
+	public static AttributeDAO getInstance() {
 		if(instance == null) {
-			synchronized(AttributeDAO_I.class) {
+			synchronized(AttributeDAO.class) {
 				if(instance == null) {
+					LOGGER.debug("Instantiaing AttributeDAO");
 					return new AttributeDAO();
 				}
 			}
@@ -42,6 +50,7 @@ public class AttributeDAO implements AttributeDAO_I{
 	private AttributeDAO() {
 		mapAttributes = new HashMap<>();
 		try {
+			LOGGER.debug("Reading Files");
 			listNormalAttributesFromFile();
 			listSpecialAttributesFromFile();
 		} catch (FileNotFoundException e) {
@@ -50,50 +59,54 @@ public class AttributeDAO implements AttributeDAO_I{
 	}
 
 	@Override
-	public List<AttributeI> getAllAttributes() {
+	public List<AttributeI> executeGetAllAttributes() {
 		return new ArrayList<>(mapAttributes.values());	
 	}
 
 	@Override
-	public List<AttributeI> getNormalAttributes() {
+	public List<AttributeI> executeGetNormalAttributes() {
 			return mapAttributes.values().stream().filter(a -> a.getType().toString().equals("NORMAL")).collect(Collectors.toList());		 
 	}
 
 	@Override
-	public List<AttributeI> getSpecialAttributes() {
+	public List<AttributeI> executeGetSpecialAttributes() {
 		return mapAttributes.values().stream().filter(a -> a.getType().toString().equals("SPECIAL")).collect(Collectors.toList());
 	}
 
 	@Override
-	public AttributeI getAttributeByName(String name) {
+	public AttributeI executeGetAttributeByName(String name) {
 		return mapAttributes.get(name);
 	}
 	
 	@Override
-	public void createAttribute(AttributeI attribute) {
-		if(!validateAttributeFields(attribute)) {
-			return;
-		}
+	public AttributeI executeCreateAttribute(AttributeI attribute) {
 		mapAttributes.put(attribute.getName(), attribute);
+		return mapAttributes.get(attribute.getName());
 	}
 
 	@Override
-	public void removeAttribute(AttributeI attribute) {
-		mapAttributes.remove(attribute.getName());
-	}
-
-	@Override
-	public void updateAttribute(AttributeI old, AttributeI attribute) {
-		if(!validateAttributeFields(attribute)) {
-			return;
+	public List<AttributeI> executeRemoveAttribute(List<AttributeI> attribute) {
+		for(AttributeI a : attribute) {
+			mapAttributes.remove(a.getName(), a);			
 		}
-		mapAttributes.replace(old.getName(), attribute);
+		return attribute;
+	}
+
+	@Override
+	public List<AttributeI> executeUpdateAttributes(List<AttributeI> old, List<AttributeI> attribute) {
+		int i = 0;
+		for(AttributeI o : old) {
+			executeUpdateAttribute(o, attribute.get(i));
+			i++;
+			old.removeIf(d -> d.getName().equals(o.getName()));
+			System.out.println(o.toString());
+		}
+		return (old.size() == attribute.size()) ? attribute : old ;
 	}
 	
-	private boolean validateAttributeFields(AttributeI attribute) {
-		boolean isValid = false;
-		 return (attribute == null || attribute.getName().equals("") || attribute.getDescription().equals("")) ? 
-				 isValid : !isValid; 
+	private boolean executeUpdateAttribute(AttributeI old, AttributeI attribute) {
+		mapAttributes.replace(old.getName(), attribute);
+		return mapAttributes.containsKey(attribute.getName());
 	}
 
 	private void listNormalAttributesFromFile() throws FileNotFoundException {
@@ -136,5 +149,4 @@ public class AttributeDAO implements AttributeDAO_I{
 			e.getMessage();
 		}
 	}
-
 }
