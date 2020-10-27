a477243f414f2fd92a64f4bf1b73bf3cc004b5fc
Haku Wei <hofneondeion21@gmail.com>
2020-10-27 04:43:26 -0400

    created objects and modified rarity layout

--- dw8xl/src/main/java/com/anno/dw8xl/rarity/model/Rarity.java
+++ dw8xl/src/main/java/com/anno/dw8xl/rarity/model/Rarity.java
@@ -1,8 +1,59 @@
 package com.anno.dw8xl.rarity.model;
 
-import com.fasterxml.jackson.annotation.JsonValue;
+import org.springframework.stereotype.Component;
 
-public enum Rarity {
-	@JsonValue
-	NORMAL, RARE, UNIQUE, EXTREME, SPECIAL
+import com.anno.dw8xl.view.CharacterView;
+import com.fasterxml.jackson.annotation.JsonView;
+
+@JsonView(CharacterView.Rarity.class)
+@Component
+public class Rarity implements RarityI {
+	
+	@JsonView({CharacterView.Weapon.Affinity.Rarity.class, CharacterView.Weapon.Rarity.class})
+	private String type;
+	
+	/**
+	 * @param type
+	 */
+	public Rarity(String type) {
+		super();
+		this.type = type;
+	}
+
+	@Override
+	public String getType() {
+		return type;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Rarity other = (Rarity) obj;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return  type;
+	}
+	
+	
 }
