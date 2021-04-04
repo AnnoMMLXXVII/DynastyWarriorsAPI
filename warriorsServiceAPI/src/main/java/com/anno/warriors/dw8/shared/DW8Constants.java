49c23126052f22b5100dd08728d90f862130097f
Haku Wei <hofneondeion21@gmail.com>
2021-04-03 23:42:19 -0400

    created constants class

---
+++ warriorsServiceAPI/src/main/java/com/anno/warriors/dw8/shared/DW8Constants.java
@@ -0,0 +1,83 @@
+package com.anno.warriors.dw8.shared;
+
+import com.anno.warriors.shared.ApplicationConstants;
+
+public class DW8Constants {
+	
+	
+
+	private DW8Constants() {
+		super();
+	}
+	public static final String OFFICER_IMAGES_PATH = ApplicationConstants.RESOURCE_PATH + "Officer_Images/";
+	public static final String ATTRIBUTES_NORMAL_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/attributes/normal/normal.txt";
+	public static final String ATTRIBUTES_SPECIAL_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/attributes/special/special.txt";
+
+	public static final String OFFICER_JIN_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/characters/officer/Officer-List-Jin.txt";
+	public static final String OFFICER_OTHER_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/characters/officer/Officer-List-Other.txt";
+	public static final String OFFICER_SHU_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/characters/officer/Officer-List-Shu.txt";
+	public static final String OFFICER_WEI_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/characters/officer/Officer-List-Wei.txt";
+	public static final String OFFICER_WU_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/characters/officer/Officer-List-Wu.txt";
+
+	public static final String SUBOFFICER_JIN_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/characters/sub-officer/SubOfficer-List-Jin.txt";
+	public static final String SUBOFFICER_OTHER_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/characters/sub-officer/SubOfficer-List-Other.txt";
+	public static final String SUBOFFICER_SHU_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/characters/sub-officer/SubOfficer-List-Shu.txt";
+	public static final String SUBOFFICER_WEI_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/characters/sub-officer/SubOfficer-List-Wei.txt";
+	public static final String SUBOFFICER_WU_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/characters/sub-officer/SubOfficer-List-Wu.txt";
+
+	public static final String WEAPON_DASHER_NORMAL_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/dasher/NormalWeapons.txt";
+	public static final String WEAPON_DASHER_RARE_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/dasher/RareWeapons.txt";
+	public static final String WEAPON_DASHER_UNIQUE_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/dasher/UniqueWeapons-5-Stars.txt";
+	public static final String WEAPON_DASHER_XTREME_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/dasher/XLWeapons-6-Stars.txt";
+	public static final String WEAPON_DASHER_ATTRIBUTES_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/dasher/WeaponAttributeLevel.txt";
+
+	public static final String WEAPON_DIVER_NORMAL_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/diver/NormalWeapons.txt";
+	public static final String WEAPON_DIVER_RARE_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/diver/RareWeapons.txt";
+	public static final String WEAPON_DIVER_UNIQUE_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/diver/UniqueWeapons-5-Stars.txt";
+	public static final String WEAPON_DIVER_XTREME_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/diver/XLWeapons-6-Stars.txt";
+	public static final String WEAPON_DIVER_ATTRIBUTES_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/diver/WeaponAttributeLevel.txt";
+
+	public static final String WEAPON_SHADOW_NORMAL_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/shadow/NormalWeapons.txt";
+	public static final String WEAPON_SHADOW_RARE_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/shadow/RareWeapons.txt";
+	public static final String WEAPON_SHADOW_UNIQUE_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/shadow/UniqueWeapons-5-Stars.txt";
+	public static final String WEAPON_SHADOW_XTREME_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/shadow/XLWeapons-6-Stars.txt";
+	public static final String WEAPON_SHADOW_ATTRIBUTES_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/shadow/WeaponAttributeLevel.txt";
+
+	public static final String WEAPON_WHIRLWIND_NORMAL_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/whirlwind/NormalWeapons.txt";
+	public static final String WEAPON_WHIRLWIND_RARE_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/whirlwind/RareWeapons.txt";
+	public static final String WEAPON_WHIRLWIND_UNIQUE_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/whirlwind/UniqueWeapons-5-Stars.txt";
+	public static final String WEAPON_WHIRLWIND_XTREME_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/whirlwind/XLWeapons-6-Stars.txt";
+	public static final String WEAPON_WHIRLWIND_ATTRIBUTES_FILE = ApplicationConstants.RESOURCE_PATH
+			+ "Text-files/weapons/whirlwind/WeaponAttributeLevel.txt";
+}
