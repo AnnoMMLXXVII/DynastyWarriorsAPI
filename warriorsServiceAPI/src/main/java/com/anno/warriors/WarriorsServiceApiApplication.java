32a835999492c7c10c53416bb98005e1295bcc25
Haku Wei <hofneondeion21@gmail.com>
2021-04-03 20:32:37 -0400

    initialized new project for DynastyWarriors. Added folders into resources

---
+++ warriorsServiceAPI/src/main/java/com/anno/warriors/WarriorsServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.anno.warriors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarriorsServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarriorsServiceApiApplication.class, args);
+	}
+
+}
---
+++ warriorsServiceAPI/src/test/java/com/anno/warriors/WarriorsServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anno.warriors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarriorsServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
