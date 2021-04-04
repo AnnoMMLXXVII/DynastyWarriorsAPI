57382b2b2a3eec9b2846d1ba3fe7514bc70b8591
Haku Wei <hofneondeion21@gmail.com>
2021-04-04 00:34:51 -0400

    removed rest-dw8xl folder

--- warriorsServiceAPI/src/main/java/com/anno/warriors/dw8/DynastyWarriors8Application.java
+++ warriorsServiceAPI/src/main/java/com/anno/warriors/dw8/DynastyWarriors8Application.java
@@ -1,12 +1,10 @@
 package com.anno.warriors.dw8;
 
 import com.anno.warriors.dw8.manager.DW8Manager;
-import com.anno.warriors.dw8.manager.DynastyWarriors8Object;
 
 public class DynastyWarriors8Application {
 
 	private static DynastyWarriors8Application instance;
-	private static DynastyWarriors8Object<DW8Manager> manager;
 
 	public static DynastyWarriors8Application getInstance() {
 		if (instance == null) {
@@ -19,12 +17,16 @@ public class DynastyWarriors8Application {
 		return instance;
 	}
 
-	@SuppressWarnings("unchecked")
 	private DynastyWarriors8Application() {
 		/*
 		 * Static Service that reads all files when app starts
 		 */
-
+		run();
+		
+	}
+	
+	private void run() {
+		DW8Manager.getInstance();
 	}
 
 }
