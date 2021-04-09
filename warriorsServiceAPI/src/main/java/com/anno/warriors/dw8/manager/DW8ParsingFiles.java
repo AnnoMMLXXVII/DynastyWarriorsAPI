d75f59fc2f61c1b88224bc5a935b957ec8db8553
Haku Wei <hofneondeion21@gmail.com>
2021-04-09 17:38:26 -0400

    making calls to DW8CharacterParseManager

--- warriorsServiceAPI/src/main/java/com/anno/warriors/dw8/manager/DW8ParsingFiles.java
+++ warriorsServiceAPI/src/main/java/com/anno/warriors/dw8/manager/DW8ParsingFiles.java
@@ -1,5 +1,12 @@
 package com.anno.warriors.dw8.manager;
 
+import java.util.List;
+import java.util.Map;
+
+import com.anno.warriors.dw8.characters.CharacterInterface;
+import com.anno.warriors.dw8.enums.kingdom.Kingdom;
+import com.anno.warriors.dw8.manager.characters.DW8CharacterParseManager;
+
 /*
  * Class is responsible to load and read all files
  * Will Most likely use the DW8Structures class to keep track of 
@@ -22,14 +29,28 @@ public class DW8ParsingFiles implements DynastyWarriors8Object<DW8ParsingFiles> 
 	}
 
 	private DW8ParsingFiles() {
-		
+		DW8CharacterParseManager.getInstance();
 	}
 
 	@Override
 	public DW8ParsingFiles getManagerType() {
 		return this;
 	}
-	
-	
-	
+
+	public static List<DynastyWarriors8Object<CharacterInterface>> getAllOfficers() {
+		return DW8CharacterParseManager.getOfficers();
+	}
+
+	public static List<DynastyWarriors8Object<CharacterInterface>> getAllSubOfficers() {
+		return DW8CharacterParseManager.getSubOfficers();
+	}
+
+	public static List<DynastyWarriors8Object<CharacterInterface>> getAllCharacters() {
+		return DW8CharacterParseManager.getOfficers();
+	}
+
+	public static Map<Kingdom, List<DynastyWarriors8Object<CharacterInterface>>> getMappedKingdomCharacters() {
+		return DW8CharacterParseManager.getKingdomCharacterMap();
+	}
+
 }
