98c687d593a0b7c4f25d2dca61e31907eceb6232
Haku Wei <hofneondeion21@gmail.com>
2021-04-12 21:31:32 -0400

    updated binary search to accomodate for duplicates

--- warriorsServiceAPI/src/main/java/com/anno/warriors/shared/WarriorSearcher.java
+++ warriorsServiceAPI/src/main/java/com/anno/warriors/shared/WarriorSearcher.java
@@ -1,7 +1,12 @@
 package com.anno.warriors.shared;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.anno.warriors.dw8.characters.model.Character;
+import com.anno.warriors.dw8.characters.model.CharacterInterface;
+
+@SuppressWarnings("unchecked")
 public class WarriorSearcher<T extends Comparable<T>> {
 	private List<T> list;
 
@@ -26,9 +31,50 @@ public class WarriorSearcher<T extends Comparable<T>> {
 		return -1;
 	}
 
+	private int binarySearch(List<T> list, String keyObj) {
+		int low = 0;
+		int high = list.size() - 1;
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			if (keyObj.compareTo(parseCharacterObjectToGetName(mid)) == 0) {
+				return mid;
+			}
+			if (keyObj.compareTo(parseCharacterObjectToGetName(mid)) < 0) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return -1;
+	}
+
 	public T search(T object) {
 		int idx = binarySearch(this.list, object);
 		return (idx == -1) ? null : list.get(idx);
 	}
 
+	public List<CharacterInterface<Character>> search(String object) {
+		List<CharacterInterface<Character>> temp = new ArrayList<>();
+		int idx = binarySearch(this.list, object);
+		temp.add((CharacterInterface<Character>) list.get(idx));
+		return binaryResultDuplicate(idx, object, temp);
+	}
+
+	private String parseCharacterObjectToGetName(int idx) {
+		String[] parsedStr = list.get(idx).toString().split(",");
+		String[] parsedEquals = parsedStr[0].split("=");
+		return parsedEquals[1];
+	}
+
+	private List<CharacterInterface<Character>> binaryResultDuplicate(int idx, String object, List<CharacterInterface<Character>> temp) {
+		int i = idx++;
+		if (idx != -1 && parseCharacterObjectToGetName(i).compareTo(object) == 0) {
+			temp = new ArrayList<>();
+			while (i < list.size() - 1 && parseCharacterObjectToGetName(i).compareTo(object) == 0) {
+				temp.add((CharacterInterface<Character>) list.get(i));
+				i++;
+			}
+		}
+		return temp;
+	}
 }
