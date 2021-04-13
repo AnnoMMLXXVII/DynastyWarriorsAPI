c18fe85f040540e9b64329abc03c540d11d06c65
Haku Wei <hofneondeion21@gmail.com>
2021-04-12 21:30:26 -0400

    new methods for POST

--- warriorsServiceAPI/src/main/java/com/anno/warriors/dw8/characters/facade/CharacterFacade.java
+++ warriorsServiceAPI/src/main/java/com/anno/warriors/dw8/characters/facade/CharacterFacade.java
@@ -6,28 +6,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.anno.warriors.dw8.characters.dao.CharacterDAOInterface;
+import com.anno.warriors.dw8.characters.model.Character;
 import com.anno.warriors.dw8.characters.model.CharacterInterface;
+import com.anno.warriors.dw8.manager.DW8Structures;
 import com.anno.warriors.shared.WarriorSorter;
 
 @Service("CharacterFacade")
+@SuppressWarnings({ "unchecked", "rawtypes" })
 public class CharacterFacade implements CharacterFacadeInterface {
 
 	@Autowired
 	private CharacterDAOInterface dao;
 
 	@Override
-	public List<CharacterInterface<com.anno.warriors.dw8.characters.model.Character>> callingGetAllCharacters() {
+	public List<CharacterInterface<Character>> callingGetAllCharacters() {
 		return new WarriorSorter(dao.getAllCharacters()).getSortedList();
 	}
 
 	@Override
-	public List<CharacterInterface<com.anno.warriors.dw8.characters.model.Character>> callingGetAllOfficers() {
+	public List<CharacterInterface<Character>> callingGetAllOfficers() {
 		return new WarriorSorter(dao.getAllOfficers()).getSortedList();
 	}
 
 	@Override
-	public List<CharacterInterface<com.anno.warriors.dw8.characters.model.Character>> callingGetAllSubOfficers() {
+	public List<CharacterInterface<Character>> callingGetAllSubOfficers() {
 		return new WarriorSorter(dao.getAllSubOfficers()).getSortedList();
+	}
+
+	@Override
+	public List<CharacterInterface<Character>> callingGetCharactersByNames(String[] name) {
+		List<CharacterInterface<Character>> returnedList = dao.getCharactersByName(DW8Structures.getCharacters(), name);
+		return new WarriorSorter(returnedList).getSortedList();
+	}
+
+	@Override
+	public List<CharacterInterface<Character>> callingGetOfficersByNames(String[] name) {
+		List<CharacterInterface<Character>> returnedList = dao.getCharactersByName(DW8Structures.getOfficers(), name);
+		return new WarriorSorter(returnedList).getSortedList();
+	}
+
+	@Override
+	public List<CharacterInterface<Character>> callingGetSubOfficersByNames(String[] name) {
+		List<CharacterInterface<Character>> returnedList = dao.getCharactersByName(DW8Structures.getSubOfficers(),
+				name);
+		return new WarriorSorter(returnedList).getSortedList();
 	}
 
 }
--- warriorsServiceAPI/src/main/java/com/anno/warriors/dw8/characters/facade/CharacterFacadeInterface.java
+++ warriorsServiceAPI/src/main/java/com/anno/warriors/dw8/characters/facade/CharacterFacadeInterface.java
@@ -2,14 +2,21 @@ package com.anno.warriors.dw8.characters.facade;
 
 import java.util.List;
 
+import com.anno.warriors.dw8.characters.model.Character;
 import com.anno.warriors.dw8.characters.model.CharacterInterface;
 
 public interface CharacterFacadeInterface {
 
-	public List<CharacterInterface<com.anno.warriors.dw8.characters.model.Character>> callingGetAllCharacters();
+	public List<CharacterInterface<Character>> callingGetAllCharacters();
 
-	public List<CharacterInterface<com.anno.warriors.dw8.characters.model.Character>> callingGetAllOfficers();
+	public List<CharacterInterface<Character>> callingGetAllOfficers();
 
-	public List<CharacterInterface<com.anno.warriors.dw8.characters.model.Character>> callingGetAllSubOfficers();
+	public List<CharacterInterface<Character>> callingGetAllSubOfficers();
+
+	public List<CharacterInterface<Character>> callingGetCharactersByNames(String[] name);
+
+	public List<CharacterInterface<Character>> callingGetOfficersByNames(String[] name);
+
+	public List<CharacterInterface<Character>> callingGetSubOfficersByNames(String[] name);
 
 }
