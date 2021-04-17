834ddcf56810ce1ba964964359473f00d134ff20
Haku Wei <hofneondeion21@gmail.com>
2021-04-17 12:44:51 -0400

    added missing Types and its values

--- warriorsServiceAPI/src/main/java/com/anno/warriors/dw8/enums/types/Types.java
+++ warriorsServiceAPI/src/main/java/com/anno/warriors/dw8/enums/types/Types.java
@@ -6,7 +6,7 @@ public enum Types implements DW8Enumeration<Types> {
 	SPEAR("Spear"), PIKE("Pike"), DOUBLE_VOULGE("Double Voulge"), GREAT_SWORD("Great Sword"), CLUB("Club"),
 	GLOVES("Gloves"), HALBERD("Halberd"), TWIN_AXES("Twin Axes"), LANCE("Lance"), TWIN_RODS("Twin Rods"), AXE("Axe"),
 	TONFA("Tonfa"), CROSSBOW("Crossbow"), ARM_CANNON("Arm Cannon"), BRUSH("Brush"), DAGGER_AXE("Dagger Axe"),
-	BOMB("Bomb"), SHORT_PIKE("Short Pike"), GREAT_AXES("Great Axes"), MACE("Mace"), SPIKED_SHIELD("Spiked Shield"),
+	BOMB("Bomb"), SHORT_PIKE("Short Pike"), GREAT_AXE("Great Axe"), MACE("Mace"), SPIKED_SHIELD("Spiked Shield"),
 	ARM_BLADE("Arm Blade"), CIRCLE_BLADE("Circle Blade"), SIEGE_SPEAR("Siege Spear"), BOW_ROD("Bow & Rod"),
 	WHEELED_HALBERD("Wheeled Halberd"), RAKE("Rake"), DUAL_HEADED_MACE("Dual-Headed Mace"),
 	REVOLVING_CROSSBOW("Revolving Crossbow"), WAR_TRIDENT("War Trident"), STELE("Stele"), SWORD("Sword"),
@@ -23,8 +23,10 @@ public enum Types implements DW8Enumeration<Types> {
 	GENERALS_SWORD("General's Sword"), NINE_RINGS_BLADE("Nine Rings Blade"), DOUBLE_EDGE_TRIDENT("Double-Edge Trident"),
 	SHORT_ROD("Short Rod"), JAVELIN("Javelin"), CROSSED_PIKE("Crossed Pike"), DRAGON_SPEAR("Dragon Spear"),
 	CRESCENT_BLADE("Crescent Blade"), DUAL_BLADE("Dual Blade"), HORSEHAIR_WHIP("Horsehair Whip"),
-	STRIKING_SWORD("Striking Sword"), TAILSMAN_CARDS("Tailsman Cards"), FLAIL_SWORD("Flail Sword"),
-	WOVEN_CLOTH("Woven Cloth");
+	STRIKING_SWORD("Striking Sword"), TALISMAN_CARDS("Talisman Cards"), FLAIL_SWORD("Flail Sword"),
+	WOVEN_CLOTH("Woven Cloth"), FLAMING_BOW("Flaming Bow"), MANDARIN_DUCK_HOOKS("Mandarin Duck Hooks"),
+	TWIN_PIKE("Twin Pike"), FLAMING_SWORD("Flaming Sword"), DRAGON_COLUMN("Dragon Column"), FLAIL_BLADE("Flail Blade"),
+	BLADEBOW("Bladebow"), PAIRED_FANS("Paired Fans"), EMEI_PIERCERS("Emei Piercers"), SABATONS("Sabatons");
 
 	private String value;
 
@@ -44,7 +46,7 @@ public enum Types implements DW8Enumeration<Types> {
 
 	public static Types returnCorrectEnum(String str) {
 		for (Types a : values()) {
-			if (a.name().equalsIgnoreCase(str)) {
+			if (a.getValue().equalsIgnoreCase(str)) {
 				return a;
 			}
 		}
