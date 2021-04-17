e1d23ecbdcebf7c8118696f8bebf3f1b343f3f13
Haku Wei <hofneondeion21@gmail.com>
2021-04-17 13:06:29 -0400

    new class for Weapon Attributes called Attribute Slots

---
+++ warriorsServiceAPI/src/main/java/com/anno/warriors/dw8/weapons/slots/PowerlessAttribute.java
@@ -0,0 +1,25 @@
+package com.anno.warriors.dw8.weapons.slots;
+
+import com.anno.warriors.dw8.enums.attribute.AttributeInterface;
+
+public class PowerlessAttribute extends AttributeSlot {
+
+	protected PowerlessAttribute() {
+		super();
+	}
+
+	public PowerlessAttribute(AttributeInterface attribute) {
+		super(attribute);
+	}
+
+	@Override
+	public String getState() {
+		return "Special";
+	}
+
+	@Override
+	public int compareTo(AttributeSlot o) {
+		return getName().compareTo(o.getName());
+	}
+
+}
