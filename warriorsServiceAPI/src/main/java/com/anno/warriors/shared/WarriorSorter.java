4915c61ed06fdf0a3107de6d3986586c15b85ed7
Haku Wei <hofneondeion21@gmail.com>
2021-04-17 12:49:04 -0400

    updated methods to keep in-line with generics implementation

--- warriorsServiceAPI/src/main/java/com/anno/warriors/shared/WarriorSorter.java
+++ warriorsServiceAPI/src/main/java/com/anno/warriors/shared/WarriorSorter.java
@@ -8,7 +8,12 @@ public class WarriorSorter<T extends Comparable<T>> {
 
 	public WarriorSorter(List<T> list) {
 		this.list = list;
-		quickSort(this.list, 0, this.list.size() - 1);
+		quickSort(list, 0, this.list.size() - 1);
+	}
+
+	public WarriorSorter(List<T> list, boolean ASC) {
+		this.list = list;
+		quickSortWithOrder(list, 0, this.list.size() - 1, ASC);
 	}
 
 	public List<T> getSortedList() {
@@ -28,6 +33,19 @@ public class WarriorSorter<T extends Comparable<T>> {
 		return i + 1;
 	}
 
+	private int partitionWithOrder(List<T> list, int low, int high, boolean ASC) {
+		T pivot = list.get(high);
+		int i = low - 1;
+		for (int j = low; j <= high - 1; j++) {
+			if (compareWithOrder(j, list, pivot, ASC)) {
+				i++;
+				swap(list, i, j);
+			}
+		}
+		swap(list, i + 1, high);
+		return i + 1;
+	}
+
 	private void quickSort(List<T> list, int low, int high) {
 		if (low < high) {
 			int pi = partition(list, low, high);
@@ -36,10 +54,24 @@ public class WarriorSorter<T extends Comparable<T>> {
 		}
 	}
 
+	private void quickSortWithOrder(List<T> list, int low, int high, boolean ASC) {
+		if (low < high) {
+			int pi = partitionWithOrder(list, low, high, ASC);
+			quickSortWithOrder(list, low, pi - 1, ASC);
+			quickSortWithOrder(list, pi + 1, high, ASC);
+		}
+	}
+
 	private void swap(List<T> list, int first, int second) {
 		T temp = list.get(first);
 		list.set(first, list.get(second));
 		list.set(second, temp);
+	}
+
+	private boolean compareWithOrder(int idx, List<T> list, T pivot, boolean ASC) {
+		return (ASC) ? list.get(idx).compareTo(pivot) < 0 :
+				list.get(idx).compareTo(pivot) > 0;
+
 	}
 
 }
