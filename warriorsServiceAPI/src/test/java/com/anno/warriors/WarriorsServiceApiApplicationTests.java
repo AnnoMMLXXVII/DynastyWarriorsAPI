5bfd29deac4730b724cf37d97cbc2babf85c0b3b
Haku Wei <hofneondeion21@gmail.com>
2021-04-24 21:22:18 -0400

    empty class

--- warriorsServiceAPI/src/test/java/com/anno/warriors/WarriorsServiceApiApplicationTests.java
+++ warriorsServiceAPI/src/test/java/com/anno/warriors/WarriorsServiceApiApplicationTests.java
@@ -1,13 +1,5 @@
 package com.anno.warriors;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class WarriorsServiceApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
