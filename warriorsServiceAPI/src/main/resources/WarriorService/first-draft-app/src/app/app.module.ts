56ca43dce327a7988a7b6295ad15c1fd4abad492
Haku Wei <hofneondeion21@gmail.com>
2021-05-23 18:34:00 -0400

    added some sample info

--- warriorsServiceAPI/src/main/resources/WarriorService/first-draft-app/src/app/app.module.ts
+++ warriorsServiceAPI/src/main/resources/WarriorService/first-draft-app/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { MercenaryComponent } from './mercenary/mercenary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    MercenaryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
