0f2eaa43b8b738ecaf21cf15ede4e6028b722e77
Tess Tickle <annommlxxvii@TessTickle.local>
2024-09-28 04:30:53 -0400

    updated CORS UI url

--- warriorsServiceAPI/src/main/java/com/anno/warriors/shared/ApplicationConstants.java
+++ warriorsServiceAPI/src/main/java/com/anno/warriors/shared/ApplicationConstants.java
@@ -10,5 +10,5 @@ public class ApplicationConstants {
 	public static final String MAIN_JAVA_PATH = "src/main/java/";
 	public static final String TEST_PATH = "src/test/java/";
 	public static final String CROSS_ORIGIN_LOCAL_HOST = "http://localhost:4200";
-	public static final String LEGENDARY_WARRIORS_NETLIFY = "https://legendary-warriors-xl.netlify.app/";
+	public static final String LEGENDARY_WARRIORS_NETLIFY = "https://legendary-warriors-xl.netlify.app";
 }
